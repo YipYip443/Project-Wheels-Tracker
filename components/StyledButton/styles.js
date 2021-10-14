7d076757912d2fcac87a849e8160a8bb1880a654
Tythor <tthorin@live.com>
2021-10-14 12:56:46 -0700

    borderRadius Fix

--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
     button:{
         backgroundColor:'white',
         height: 40,
-        borderRadius:'20px',
+        borderRadius: 20,
         justifyContent: 'center',
         alignItems:'center'
 
