965eb16bb4d5ce701084002a03b16c45b61774d4
Helen To <tohelen21@gmail.com>
2021-10-21 12:00:36 -0700

    created scheduler template

--- App.js
+++ App.js
@@ -2,32 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ImageBackground, StyleSheet, Text, View, Platform } from 'react-native';
 import TitleItem from './components/TitleComponents/TitleScreen';
+import ScheduleItem from './components/TitleComponents/Scheduler';
 import LoginScreen from "./components/TitleComponents/LoginScreen";
 
-import * as firebase from '@react-native-firebase/app'
+
+// import * as firebase from '@react-native-firebase/app'
 
 
-const credentials = {
-  apiKey: "FIREBASE_API_KEY",
+// const credentials = {
+//   apiKey: "FIREBASE_API_KEY",
 
-  authDomain: "wheels-tracker-25085.firebaseapp.com",
+//   authDomain: "wheels-tracker-25085.firebaseapp.com",
 
-  projectId: "wheels-tracker-25085",
+//   projectId: "wheels-tracker-25085",
 
-  storageBucket: "wheels-tracker-25085.appspot.com",
+//   storageBucket: "wheels-tracker-25085.appspot.com",
 
-};
+// };
 
-await firebase.initializeApp(credentials);
-
-
+// await firebase.initializeApp(credentials);
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      {/*<TitleItem />*/}
-      <LoginScreen />
+      {/* <TitleItem />
+      <LoginScreen /> */}
+      <ScheduleItem/>
 
       <StatusBar style="auto" />
   </View>
@@ -38,8 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
