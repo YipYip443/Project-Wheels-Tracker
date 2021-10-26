e0078dbec9168bd2ad6b2dd1d9f7a7d047ab5245
Helen To <tohelen21@gmail.com>
2021-10-26 11:57:43 -0700

    delete ItsYourBirthday

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import React from 'react';
 import { ImageBackground, StyleSheet, Text, View, Platform } from 'react-native';
 import TitleItem from './components/TitleComponents/TitleScreen';
 import LoginScreen from "./components/TitleComponents/LoginScreen";
-import ItsYourBirthday from './components/DeliveryScheduleComponents/CreateScheduleButton';
 
 // import * as firebase from '@react-native-firebase/app'
 
@@ -30,7 +29,6 @@ export default function App() {
       {/* <TitleItem />
       <LoginScreen /> 
       <ScheduleItem/> */}
-      <ItsYourBirthday/>
 
       <StatusBar style="auto" />
   </View>
