e9b800bffa028148b13e6eab5e1a064719a6de04
Helen To <tohelen21@gmail.com>
2021-12-03 11:46:44 -0800

    infinite loop problem
    need to fix

--- components/SMDashboardComponenents/VolunteersScreen/index.js
+++ components/SMDashboardComponenents/VolunteersScreen/index.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "../../TitleComponents/LoginScreen/styles";
-import {ScrollView} from "react-native";
+import {ScrollView, View, Text} from "react-native";
+import {db} from "../../../db/firestore";
 
-const VolunteersScreen = ({navigation}) => {
+const VolunteersScreen = () => {
+    const [userList, setUserList] = useState([]);
+
+    //TODO: fix infinite loop (it prints user's name & phone inifintely on the screen)
+    //TODO: styling is left for Cam
+    function getUser() {
+        db.collection('users').where('isAdmin', '==', false).get().then(snapshot => {
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                return;
+            }
+            snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data().name);
+                // var key = doc.id;
+                // var uName = key.data().name;
+                // usersList.push()
+                setUserList(usersList => [...usersList, doc.data()])
+            });
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+        });
+    }
+    
+    getUser();
+
+    const showUserInformation = () => {
+        let usersInformation = userList.map((user) => (
+          <View>
+            <Text>{user.name}</Text>
+            <Text>{user.phone}</Text>
+          </View>
+        ));
+        console.log(usersInformation);
+        return usersInformation;
+    };
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
     return (
         <ScrollView
             style={styles.container}
             contentContainerStyle={styles.containerStyle}
             keyboardShouldPersistTaps={'always'}>
+        <View>
+            <Text> {showUserInformation()} </Text>
+        </View>
         </ScrollView>
     );
 }
