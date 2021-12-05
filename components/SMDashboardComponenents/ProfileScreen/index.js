f90b7c5f067faeaf42f0ceed6bb5e2e7343d152f
THESUPP0RTER <thesupprter@gmail.com>
2021-12-05 13:28:13 -0800

    Bug - Navigation issue on logout function

--- components/SMDashboardComponenents/ProfileScreen/index.js
+++ components/SMDashboardComponenents/ProfileScreen/index.js
@@ -1,7 +1,24 @@
 import React from "react";
 import styles from "../../SMDashboardComponenents/EditProfileScreen/styles";
-import {View, ScrollView, Pressable} from "react-native";
+import {View, ScrollView, Pressable, Text} from "react-native";
 import StyledButton from "../../TitleComponents/StyledButton";
+
+import { auth } from "../../../db/firestore";
+
+
+function logout() {
+    navigation = useNavigation()
+    auth.signOut()
+        .then((result) => {
+            console.log(result)
+            console.log('User signed out sucecssfully')
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    //TODO: Fix navigation bug
+    navigation.navigate('Title')
+}
 
 const ProfileScreen = ({navigation}) => {
     return (
@@ -15,6 +32,9 @@ const ProfileScreen = ({navigation}) => {
                     text={'Edit Profile Screen'}
                     onPress={() => navigation.navigate('Edit Profile')}
                 />
+                <Pressable onPress={ logout }>
+                    <Text style={styles.footer}>Log off</Text>
+                </Pressable>
             </View>
         </ScrollView>
     );
